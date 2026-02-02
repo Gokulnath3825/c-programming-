426dbcc07060e5c334a08b031bbbeede5eea4b80
Gokulnath <gokulj172@gmail.com>
2026-02-02 09:22:44 +0530

    Add files via upload
    solved 8 problem

---
+++ Array_!D/problem08.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int k;
+    scanf("%d",&k);
+    for(int j=k;j<n;j++){
+        printf("%d ",arr[j]);
+    }
+    for(int j=0;j<k;j++){
+        printf("%d ",arr[j]);
+    }
+
+    
+
+
+
+
+
+
+
+    return 0;
+}
+
+
+
