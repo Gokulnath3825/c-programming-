62f597e2e4df926f609a48fd339053a7d7039bd0
Gokulnath <gokulj172@gmail.com>
2026-02-09 23:39:34 +0530

    Add files via upload
    Problem e 11
     solved

---
+++ Strings/eproblem11.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[20];
+    fgets(str,sizeof(str),stdin);
+    int digit=0;
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]>='0' && str[i]<='9'){
+            digit++;
+        }
+    }
+    printf("%d",digit);
+}
