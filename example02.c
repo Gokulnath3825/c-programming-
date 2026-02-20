20fb0de0a426cef8d10e4658f66ab3adc7ffa441
Gokulnath <gokulj172@gmail.com>
2026-02-20 21:55:18 +0530

    Add files via upload
    solved 2 problem

---
+++ example02.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int r;
+    int c;
+    scanf("%d",&r);
+    scanf("%d",&c);
+    int a[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    
+    for(int i=0;i<r;i++){
+        int sum=0;
+        for(int j=0;j<c;j++){
+            sum+=a[i][j];
+        }
+        printf("%d ",sum);
+    }
+
+
+
+
+
+    return 0;
+}
